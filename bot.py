faacd8fce44d2593fd973dff2a3f28e25bcbbfaa
Serhii Dmitrakov <clamdm@gmail.com>
2019-08-09 12:47:49 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import telegram
+import os
+import logging
+import time
+
+logging.basicConfig(format = u'%(filename)s[LINE:%(lineno)d]-8s [%(asctime)s]  %(message)s', level = logging.DEBUG)
+
+
+def request_api(url, headers, params):
+  response = requests.get(url, headers=headers, params=params)
+  response.raise_for_status()
+  return response.json()
+
+
+def send_bot_message(negative_attempt, bot, chat_id):
+  bot.send_message(chat_id=chat_id, text="У вас проверили работу 'Отправляем уведомления о проверке работ'")
+  if negative_attempt:
+    bot.send_message(chat_id=chat_id, text="К сожалению, в работе нашлись ошибки.")
+  else:
+    bot.send_message(chat_id=chat_id, text="Преподавателю понравилось, можно приступать к следующему уроку.")
+
+
+def main():
+  url = 'https://dvmn.org/api/long_polling/'
+  devman_token = os.getenv("DEVMAN_TOKEN")
+
+  telegram_token = os.getenv("TELEGRAM_TOKEN")
+  bot = telegram.Bot(token=telegram_token)
+  chat_id = 323863021
+
+  headers = {"Authorization": devman_token}
+  params = {}
+
+  while True:
+    try:
+      data = request_api(url, headers=headers, params=params)
+    except requests.exceptions.ReadTimeout as e:
+      logging.error(e)
+      continue
+    except requests.exceptions.ConnectionError as errc:
+      logging.error("Error Connecting:", errc)
+      time.sleep(10)
+
+    if data['status'] == 'found':
+      last_tsmp = data['last_attempt_timestamp']
+      params={'timestamp': last_tsmp}
+      negative_attempt = data['new_attempts']['is_negative']
+      send_bot_message(negative_attempt, bot, chat_id)
+    else:
+      tsmp = data['timestamp_to_request']
+      params={'timestamp': tsmp}
+
+
+if __name__ == '__main__':
+  main()
